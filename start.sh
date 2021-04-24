c10ade29d7903fd929b50d91233ab665552e55ab
Jake Keeys <jake@keeys.org>
2021-04-24 18:06:29 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ORGANIZATION=$ORGANIZATION
+ACCESS_TOKEN=$ACCESS_TOKEN
+
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+
+cd /home/docker/actions-runner
+
+CONFIG_OPTS=""
+if [[ -n $RUNNER_LABELS ]]; then
+    CONFIG_OPTS="${CONFIG_OPTS} --labels ${RUNNER_LABELS}"
+fi
+
+./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} ${CONFIG_OPTS}
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
